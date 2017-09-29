ae2da27ee954b0c06c3536c9668073ddf9349049
ClaudiaEsteffani <huacacclaudia@gmail.com>
2017-09-29 13:41:26 -0500

    practica1

---
+++ Practica1/ejercicio5.R
@@ -0,0 +1,19 @@
+# Nombre: Claudia Huacac Chiri 20140516D
+# Respuesta 5
+
+tst4 <- function(x){
+  
+  if (x<-2) "muy negativo"
+  else if (x<1) "cercano a cero"
+  else if (x<3) "in [1,3)"
+  else "large"
+}
+
+tst4(0)
+tst4(Inf)
+tst4(2)
+
+Tst4 <- function(x){
+  if ()
+}
+
